2e41ce2a6bf8884abec043b52ec795412182c5d7
hoangthongnguyen06 <nguyenhoangthong06@gmail.com>
2023-12-27 07:17:11 +0700

    Update api login

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -6,8 +6,8 @@ from app.models import db
 
 auth_bp = Blueprint('auth', __name__)
 
-@auth_bp.route('/auth/login', methods=['POST'])
-def login():
+@auth_bp.route('/auth/user', methods=['POST'])
+def login_user():
     data = request.get_json()
     username = data.get('username')
     password = data.get('password')
@@ -21,6 +21,21 @@ def login():
     else:
         return jsonify({'message': 'Invalid credentials'}), 401
 
+@auth_bp.route('/auth/admin', methods=['POST'])
+def login_admin():
+    data = request.get_json()
+    username = data.get('username')
+    password = data.get('password')
+
+    user = User.query.filter_by(username=username).first()
+
+    # if user and check_password_hash(user.password, password):
+    if user and user.password==password:
+        access_token = create_access_token(identity={'id': user.id, 'username': user.username, 'role': user.role})
+        return jsonify(access_token=access_token, user_id=user.id, username=user.username, role=user.role)
+    else:
+        return jsonify({'message': 'Invalid credentials'}), 40
+    
 @auth_bp.route('/auth/logout', methods=['POST'])
 @jwt_required()
 def logout():

55b3a9de139fe9b9dd4f720f346bb0a80a36bf0f
hoangthongnguyen06 <nguyenhoangthong06@gmail.com>
2023-12-27 14:17:55 +0700

    change token life-time

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -3,6 +3,7 @@ from flask_jwt_extended import JWTManager, create_access_token, jwt_required, ge
 from werkzeug.security import check_password_hash
 from app.models.user import User
 from app.models import db
+from datetime import timedelta
 
 auth_bp = Blueprint('auth', __name__)
 
@@ -16,7 +17,8 @@ def login_user():
 
     # if user and check_password_hash(user.password, password):
     if user and user.password==password:
-        access_token = create_access_token(identity={'id': user.id, 'username': user.username, 'role': user.role})
+        expires = timedelta(days=1)
+        access_token = create_access_token(identity={'id': user.id, 'username': user.username, 'role': user.role}, expires_delta=expires)
         return jsonify(access_token=access_token, user_id=user.id, username=user.username, role=user.role)
     else:
         return jsonify({'message': 'Invalid credentials'}), 401
